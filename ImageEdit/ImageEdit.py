79b7d8b458d04c4bf2fe7f7b42122d7c9742cad6
ToxicFrazzles <frazzles740@gmail.com>
2020-08-25 12:39:13 +0100

    Added vertical flip command
    Added imagevflip command to perform a vertical flip on images in a message

--- ImageEdit/ImageEdit.py
+++ ImageEdit/ImageEdit.py
@@ -1,16 +1,44 @@
 from redbot.core import commands
 import re
 from .utils import *
+import aiohttp
+import io
+import discord
 
 
 class ImageEdit(commands.Cog):
 	"""Image editing cog"""
+	aiohttp_session = aiohttp.ClientSession()
+
+	async def extract_image_urls(self, message):
+		message_urls = re.findall(r"https?://(?:[^/\s]{1,253})/(?:[\S]{1,1800})", message.content)
+		attachment_urls = [attachment.url for attachment in message.attachments]
+		image_urls = [url for url in message_urls if await url_is_image(self.aiohttp_session, url)]
+		image_urls += [url for url in attachment_urls if await url_is_image(self.aiohttp_session, url)]
+		return image_urls
+
+	async def upload_image(self, channel, byte_buffer: io.BytesIO):
+		await channel.send(file=discord.File(byte_buffer, 'result.jpg'))
 
 	@commands.command()
 	async def imageecho(self, ctx):
 		"""Extract images from a message and send the URLs back"""
-		message_urls = re.findall(r"https?://(?:[^/\s]{1,253})/(?:[\S]{1,1800})", ctx.message.content)
-		attachment_urls = [attachment.url for attachment in ctx.message.attachments]
-		image_urls = [url for url in message_urls if url_is_image(url)]
-		image_urls += [url for url in attachment_urls if url_is_image(url)]
+		image_urls = await self.extract_image_urls(ctx.message)
 		await ctx.send(f"Found {len(image_urls)} images associated with your message:\n" + "\n".join(image_urls))
+
+	@commands.command()
+	async def imagevflip(self, ctx):
+		"""Extract images from a message and send them back but flipped vertically"""
+		image_urls = await self.extract_image_urls(ctx.message)
+		images = []
+		for url in image_urls:
+			images.append(await image_from_url(self.aiohttp_session, url))
+		async with ctx.typing():
+			for image in images:
+				image = await flip_image(image, 'v')
+				byte_buf = await image_to_bytesio(image)
+				await self.upload_image(ctx.channel, byte_buf)
+
+
+	def cog_unload(self):
+		self.aiohttp_session.close()
--- ImageEdit/utils.py
+++ ImageEdit/utils.py
@@ -1,6 +1,38 @@
-import requests
+import aiohttp
+import cv2
+import numpy as np
+from io import BytesIO
 
 
-def url_is_image(url):
-	with requests.head(url) as r:
-		return r.headers['content-type'].startswith("image/")
+async def url_is_image(session: aiohttp.ClientSession, url: str) -> bool:
+	resp = await session.head(url)
+	async with resp:
+		status = resp.status
+		reason = resp.reason
+		content_type = resp.headers['content-type']
+	if status == 405:
+		# HEAD request not allowed
+		return False
+	return content_type.startswith("image/")
+
+
+async def image_from_url(session: aiohttp.ClientSession, url: str):
+	resp = await session.get(url)
+	async with resp:
+		status = resp.status
+		reason = resp.reason
+		content_type = resp.headers['content-type']
+		content = await resp.read()
+	np_arr = np.frombuffer(content, dtype=np.uint8)
+	np_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+	return np_img
+
+
+async def image_to_bytesio(np_img):
+	return BytesIO(cv2.imencode(".jpg", np_img)[1].tobytes())
+
+
+async def flip_image(np_img, direction='v'):
+	dir_param = 0 if direction == 'v' else (1 if direction == 'h' else -1)
+	result = cv2.flip(np_img, dir_param)
+	return result
