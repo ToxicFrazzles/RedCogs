4041abe48557c97b50928147de5135b3f00c216b
ToxicFrazzles <frazzles740@gmail.com>
2020-08-26 12:48:28 +0100

    Updated jpegify and flip commands
    Updated jpegify and flip commands to use pillow for image processing while simultaneously improving the jegify to support pngs and changing the upload system

--- ImageEdit/ImageEdit.py
+++ ImageEdit/ImageEdit.py
@@ -5,6 +5,7 @@ import aiohttp
 import io
 import discord
 import asyncio
+from PIL import Image
 
 
 class ImageEdit(commands.Cog):
@@ -18,8 +19,12 @@ class ImageEdit(commands.Cog):
 		image_urls += [url for url in attachment_urls if await url_is_image(self.aiohttp_session, url)]
 		return image_urls
 
-	async def upload_image(self, channel, byte_buffer: io.BytesIO):
-		await channel.send(file=discord.File(byte_buffer, 'result.jpg'))
+	async def images_from_message(self, message):
+		image_urls = await self.extract_image_urls(message)
+		return [await image_from_url(self.aiohttp_session, url) for url in image_urls]
+
+	async def upload_image(self, channel, byte_buffer: io.BytesIO, extension=".jpg"):
+		await channel.send(file=discord.File(io.BytesIO(byte_buffer.getvalue()), "result"+extension))
 
 	@commands.command()
 	async def imageecho(self, ctx):
@@ -30,41 +35,34 @@ class ImageEdit(commands.Cog):
 	@commands.command()
 	async def imagevflip(self, ctx):
 		"""Extract images from a message and send them back but flipped vertically"""
-		image_urls = await self.extract_image_urls(ctx.message)
-		images = []
-		for url in image_urls:
-			images.append(await image_from_url(self.aiohttp_session, url))
+		images = self.images_from_message(ctx.message)
 		async with ctx.typing():
 			for image in images:
 				image = await flip_image(image, 'v')
-				byte_buf = await image_to_bytesio(image)
+				byte_buf = await encode_img(image)
 				await self.upload_image(ctx.channel, byte_buf)
 
 	@commands.command()
 	async def imagehflip(self, ctx):
 		"""Extract images from a message and send them back but flipped horizontally"""
-		image_urls = await self.extract_image_urls(ctx.message)
-		images = []
-		for url in image_urls:
-			images.append(await image_from_url(self.aiohttp_session, url))
+		images = self.images_from_message(ctx.message)
 		async with ctx.typing():
 			for image in images:
 				image = await flip_image(image, 'h')
-				byte_buf = await image_to_bytesio(image)
+				byte_buf = await encode_img(image)
 				await self.upload_image(ctx.channel, byte_buf)
 
 	@commands.command()
 	async def jpegify(self, ctx):
 		"""Extract images from the message and \"deep fry\" them before sending them back"""
-		image_urls = await self.extract_image_urls(ctx.message)
-		images = []
-		for url in image_urls:
-			images.append(await image_from_url(self.aiohttp_session, url))
+		images = self.images_from_message(ctx.message)
 		async with ctx.typing():
 			for image in images:
 				image = await jpegify_image(image)
-				byte_buf = await image_to_bytesio(image)
-				await self.upload_image(ctx.channel, byte_buf)
+				print(image.getbands(), image.size)
+				byte_buf = await encode_img(image)
+				print("Bytes 2 electric boogaloo:", len(byte_buf.getvalue()))
+				await self.upload_image(ctx.channel, byte_buf, ".png" if len(image.getbands()) == 4 else ".jpg")
 
 	@commands.command()
 	async def topcaption(self, ctx, *, message_content: str):
@@ -73,13 +71,11 @@ class ImageEdit(commands.Cog):
 		for url in image_urls:
 			caption = caption.replace(url, "")
 		caption = caption.strip()
-		images = []
-		for url in image_urls:
-			images.append(await image_from_url(self.aiohttp_session, url))
+		images = [await image_from_url(self.aiohttp_session, url) for url in image_urls]
 		async with ctx.typing():
 			for image in images:
 				image = await top_caption_image(image, caption)
-				byte_buf = await image_to_bytesio(image)
+				byte_buf = await encode_img(image)
 				await self.upload_image(ctx.channel, byte_buf)
 
 	@commands.command()
@@ -89,13 +85,11 @@ class ImageEdit(commands.Cog):
 		for url in image_urls:
 			caption = caption.replace(url, "")
 		caption = caption.strip()
-		images = []
-		for url in image_urls:
-			images.append(await image_from_url(self.aiohttp_session, url))
+		images = [await image_from_url(self.aiohttp_session, url) for url in image_urls]
 		async with ctx.typing():
 			for image in images:
 				image = await bottom_caption_image(image, caption)
-				byte_buf = await image_to_bytesio(image)
+				byte_buf = await encode_img(image)
 				await self.upload_image(ctx.channel, byte_buf)
 
 	def cog_unload(self):
--- ImageEdit/utils.py
+++ ImageEdit/utils.py
@@ -2,6 +2,7 @@ import aiohttp
 import cv2
 import numpy as np
 from io import BytesIO
+from PIL import Image
 
 
 async def url_is_image(session: aiohttp.ClientSession, url: str) -> bool:
@@ -16,41 +17,55 @@ async def url_is_image(session: aiohttp.ClientSession, url: str) -> bool:
 	return content_type.startswith("image/")
 
 
-async def image_from_url(session: aiohttp.ClientSession, url: str):
+async def image_from_url(session: aiohttp.ClientSession, url: str) -> Image.Image:
 	resp = await session.get(url)
 	async with resp:
 		status = resp.status
 		reason = resp.reason
 		content_type = resp.headers['content-type']
 		content = await resp.read()
-	np_arr = np.frombuffer(content, dtype=np.uint8)
-	np_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-	return np_img
-
-
-async def image_to_bytesio(np_img):
-	return BytesIO(await encode_jpg(np_img))
-
-
-async def flip_image(np_img, direction='v'):
-	dir_param = 0 if direction == 'v' else (1 if direction == 'h' else -1)
-	result = cv2.flip(np_img, dir_param)
-	return result
-
-
-async def jpegify_image(np_img):
-	byte_buf = await encode_jpg(np_img, quality=5)
+	byte_buf = BytesIO(content)
 	return await decode_img(byte_buf)
 
 
-async def encode_jpg(np_img, quality=95):
-	params = [cv2.IMWRITE_JPEG_QUALITY, quality]
-	return cv2.imencode(".jpg", np_img, params=params)[1].tobytes()
+async def flip_image(pil_img: Image.Image, direction='v') -> Image.Image:
+	dir_param = Image.FLIP_TOP_BOTTOM if direction == 'v' else (Image.FLIP_LEFT_RIGHT if direction == 'h' else Image.ROTATE_180)
+	return pil_img.transpose(dir_param)
 
 
-async def decode_img(byte_buf):
-	np_arr = np.frombuffer(byte_buf, dtype=np.uint8)
-	return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+async def jpegify_image(pil_img: Image.Image) -> Image.Image:
+	bands = pil_img.getbands()
+	if len(bands) == 4:
+		alpha = pil_img.getchannel("A")
+		triband = pil_img.convert(mode="RGB")
+		byte_buf = await encode_jpg(triband, quality=5)
+		triband = await decode_img(byte_buf)
+		triband.putalpha(alpha)
+		return triband
+	byte_buf = await encode_jpg(pil_img, quality=5)
+	return await decode_img(byte_buf)
+
+
+async def encode_img(pil_img: Image.Image) -> BytesIO:
+	byte_buf = BytesIO()
+	bands = pil_img.getbands()
+	if len(bands) == 4:
+		im_format = "PNG"
+	else:
+		im_format = "JPEG"
+	pil_img.save(byte_buf, format=im_format)
+	return byte_buf
+
+
+async def encode_jpg(pil_img: Image.Image, quality=95) -> BytesIO:
+	byte_buf = BytesIO()
+	pil_img.save(byte_buf, format="jpeg", quality=quality, optimize=True, progressive=True)
+	return byte_buf
+
+
+async def decode_img(byte_buf: BytesIO) -> Image.Image:
+	pil_img = Image.open(byte_buf)
+	return pil_img
 
 
 async def top_caption_image(np_img: np.ndarray, caption):
